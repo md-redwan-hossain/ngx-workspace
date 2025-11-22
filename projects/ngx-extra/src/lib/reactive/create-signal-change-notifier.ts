b88de94d7807a3b6710c21f178d7e0d18fec63b3
md-redwan-hossain <redwan.ping@gmail.com>
2025-11-22 15:55:26 +0600

    misc type change

--- projects/ngx-extra/src/lib/reactive/create-signal-change-notifier.ts
+++ projects/ngx-extra/src/lib/reactive/create-signal-change-notifier.ts
@@ -1,12 +1,17 @@
-import { signal } from "@angular/core";
+import { Signal, signal } from "@angular/core";
+
+export type SignalChangeNotifier = {
+  notify: () => void;
+  listen: Signal<number | null>;
+};
 
 export function createSignalChangeNotifier() {
   const sourceSignal = signal<number | null>(null);
 
-  return {
+  return Object.seal({
     notify: () => {
       sourceSignal.update((v) => (v === null ? 1 : v + 1));
     },
     listen: sourceSignal.asReadonly()
-  };
+  }) satisfies SignalChangeNotifier;
 }
