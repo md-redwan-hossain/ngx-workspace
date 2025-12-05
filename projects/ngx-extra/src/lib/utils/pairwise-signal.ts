eedb0ab4007c50a3aecde3f8b834be00634878ef
md-redwan-hossain <redwan.ping@gmail.com>
2025-12-06 01:06:27 +0600

    add pairwiseSignal utility function for signal management

---
+++ projects/ngx-extra/src/lib/utils/pairwise-signal.ts
@@ -0,0 +1,43 @@
+import {
+  assertInInjectionContext,
+  inject,
+  Injector,
+  isDevMode,
+  linkedSignal,
+  runInInjectionContext,
+  Signal,
+  ValueEqualityFn
+} from "@angular/core";
+
+type Value<T> = { current: T; previous: T | null };
+
+type Opts<T> = {
+  equalityFn?: ValueEqualityFn<Value<T>>;
+  injector?: Injector;
+};
+
+export function pairwiseSignal<T>(source: Signal<T>, options?: Opts<T>) {
+  if (isDevMode() && !options?.injector) {
+    assertInInjectionContext(pairwiseSignal);
+  }
+  const assertedInjector = options?.injector ?? inject(Injector);
+
+  const defaultEqualityFn = (a: Value<T>, b: Value<T>) =>
+    JSON.stringify(a.previous) === JSON.stringify(b.current);
+
+  return runInInjectionContext(assertedInjector, () => {
+    const pair = linkedSignal<T, Value<T>>({
+      source: source,
+      equal: options?.equalityFn ?? defaultEqualityFn,
+      computation: (current, previous) => {
+        console.log(JSON.stringify(previous) === JSON.stringify(current));
+        return {
+          current,
+          previous: previous?.value.current ?? null
+        };
+      }
+    });
+
+    return pair.asReadonly();
+  });
+}
