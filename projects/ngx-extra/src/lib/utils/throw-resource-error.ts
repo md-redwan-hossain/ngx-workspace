1418fb3647551f04a5920a89dd5369a51dfdb009
md-redwan-hossain <redwan.ping@gmail.com>
2026-03-16 14:54:33 +0600

    docs: update documentation for throwResourceError function

--- projects/ngx-extra/src/lib/utils/throw-resource-error.ts
+++ projects/ngx-extra/src/lib/utils/throw-resource-error.ts
@@ -8,6 +8,15 @@ import {
   isDevMode
 } from "@angular/core";
 
+/**
+ * Subscribe to a resource and re-throw its error inside an Angular effect.
+ *
+ * Important: Before accessing the `value()` of a resource, always do conditional check by `hasValue()`.
+ * Otherwise `throwResourceError` can lead to infinite loop.
+ *
+ * @param resorce The `HttpResourceRef<T>` to observe for errors.
+ * @param injector Optional `Injector` to run the effect in (defaults to current injection context).
+ */
 export function throwResourceError<T = any>(resorce: HttpResourceRef<T>, injector?: Injector) {
   if (isDevMode() && !injector) {
     assertInInjectionContext(throwResourceError);
